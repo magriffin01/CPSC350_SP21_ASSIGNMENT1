9e71dcee853552072839ce56dc6163499db9e414
Mark Griffin <magriffin@chapman.edu>
2021-02-07 02:12:51 +0000

    Added empty header and cpp files

---
+++ Model.h
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Model
+{
+    public:
+        Model();
+        ~Model();
+        string translateSingleCharacter(char character);
+        string translateDoubleCharacter(char character);
+
+    private:
+
+};
